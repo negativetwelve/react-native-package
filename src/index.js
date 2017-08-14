3064f4edf2fbfbe4f765fd90b58972074864d274
Mark Miyashita <negativetwelve@gmail.com>
2017-08-14 03:00:16 -0700

    Implement createPackage and export as Package.create using check and guard

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// Libraries
+import {check} from 'react-native-module-check';
+import {guard} from 'react-native-module-guard';
+
+
+const createPackage = ({
+  // This is the package.json for the module.
+  json,
+  nativeModule,
+  enabled,
+  export: createExport,
+}) => {
+  const {name, homepage: repo} = json;
+
+  // `enabled` might be passed in as undefined (from Platform.select) so we
+  // can't use default args here.
+  const isEnabled = enabled || false;
+
+  // Runs checks to make sure the module is installed correctly if `isEnabled`
+  // is set to true.
+  check({name, repo, nativeModule, enabled: isEnabled});
+
+  // Returns the guarded, exported module from the input `export` function.
+  return guard({
+    name,
+    repo,
+    nativeModule,
+    enabled: isEnabled,
+    export: createExport,
+  });
+};
+
+
+module.exports = {
+  create: createPackage,
+};
